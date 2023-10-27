da9c567c0a56a248f699db5357c83e40df2840dc
Eduardo-lima92 <dudulima2005@hotmail.com>
2023-10-27 13:41:56 -0300

    Criação do esquema de livros

---
+++ src/models/livro.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const esquema = new mongoose.Schema(
+    {
+        titulo: {
+            type: String,
+            required: 'é obrigatorio',
+        },
+        qtdPaginas: {
+            type: Number,
+            required: 'é obrigatorio',
+        },
+
+        codigoISBN: {
+            type: Number,
+            default: '',
+        },
+        
+        editora: {
+            type: String,
+            required: 'é obrigatorio',
+        },
+
+    },
+    {
+        timestamps: true
+    }
+);
+
+const EsquemaLivro = mongoose.models.Livro || mongoose.model('Livro', esquema);
+module.exports = EsquemaLivro;
